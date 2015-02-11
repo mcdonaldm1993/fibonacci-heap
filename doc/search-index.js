2a7c8790076cdf5e30da5e5cebc4d12f5a5fa37f
Michael McDonald <mcdonaldm1993@gmail.com>
2015-02-11 07:08:04 +0000

    - Updated docs to commit: 2d8dabd2c2da2fca1cf8c7ae9ca5c44bf7e1e934

--- doc/search-index.js
+++ doc/search-index.js
@@ -1,5 +1,3 @@
 var searchIndex = {};
-searchIndex['main'] = {"items":[],"paths":[]};
 searchIndex['fibonacci_heap'] = {"items":[[0,"","fibonacci_heap",""],[3,"FibonacciHeap","","Struct that represents the [Fibonacci Heap](http://en.wikipedia.org/wiki/Fibonacci_heap) data structure."],[11,"new","","Creates a new empty `FibonacciHeap`.",0],[11,"insert","","Inserts the value into the heap with priority key.",0],[11,"minimum","","Peeks at the minimum of the heap.",0],[11,"extract_min","","Exctracts the minimum of the heap.",0],[11,"decrease_key","","Decreases the priority of the value to the key.",0]],"paths":[[3,"FibonacciHeap"]]};
-
 initSearch(searchIndex);
